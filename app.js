ecd9512801d525dcb190756438a5314edf5f4006
harshit-506 <supriyamohapatra658@gmail.com>
2024-09-05 12:23:28 +0530

    modified code

--- app.js
+++ app.js
@@ -8,52 +8,88 @@ function createShortcut() {
             {
                 WFWorkflowActionIdentifier: 'is.workflow.actions.ask',
                 WFWorkflowActionParameters: {
-                    WFAskActionPrompt: { Value: { String: 'Enter WiFi network name (SSID)' } },
-                    WFInputType: { Value: 'Text' }
+                    WFAskActionPrompt: 'Enter WiFi network name (SSID)',
+                    WFInputType: 'Text'
                 }
             },
             {
                 WFWorkflowActionIdentifier: 'is.workflow.actions.ask',
                 WFWorkflowActionParameters: {
-                    WFAskActionPrompt: { Value: { String: 'Enter WiFi password' } },
-                    WFInputType: { Value: 'Text' }
+                    WFAskActionPrompt: 'Enter WiFi password',
+                    WFInputType: 'Text'
                 }
             },
             {
                 WFWorkflowActionIdentifier: 'is.workflow.actions.text',
                 WFWorkflowActionParameters: {
-                    Text: { Value: { String: 'WIFI:S:{{Ask for Input}};;T:WPA;P:{{Ask for Input 2}};;' } }
+                    Text: {
+                        Value: {
+                            String: 'WIFI:S:{{Ask for Input}};;T:WPA;P:{{Ask for Input 2}};;'
+                        }
+                    }
                 }
             },
             {
                 WFWorkflowActionIdentifier: 'is.workflow.actions.generateqrcode',
                 WFWorkflowActionParameters: {
-                    WFInputText: { Value: { Type: 'Variable', VariableName: 'Text' } }
+                    WFInputText: {
+                        Value: {
+                            Type: 'Variable',
+                            VariableName: 'Text'
+                        }
+                    }
                 }
             },
             {
                 WFWorkflowActionIdentifier: 'is.workflow.actions.showresult',
                 WFWorkflowActionParameters: {
-                    Text: { Value: { Type: 'Variable', VariableName: 'QR Code' } }
+                    Text: {
+                        Value: {
+                            Type: 'Variable',
+                            VariableName: 'QR Code'
+                        }
+                    }
                 }
             }
         ],
-        WFWorkflowClientVersion: '1050',
+        WFWorkflowClientVersion: '754',
         WFWorkflowClientRelease: '2.1.2',
+        WFWorkflowMinimumClientVersion: 411,
         WFWorkflowIcon: {
-            WFWorkflowIconStartColor: 4282601983,
-            WFWorkflowIconGlyphNumber: 59511
+            WFWorkflowIconGlyphNumber: 59511,
+            WFWorkflowIconImageData: '',
+            WFWorkflowIconStartColor: 2071128575
         },
-        WFWorkflowImportQuestions: [],
-        WFWorkflowTypes: ['ncwidget', 'watch', 'quicklook'],
-        WFWorkflowActions: []
+        WFWorkflowTypes: ['NCWidget', 'WatchKit'],
+        WFWorkflowInputContentItemClasses: [
+            'WFAppStoreAppContentItem',
+            'WFArticleContentItem',
+            'WFContactContentItem',
+            'WFDateContentItem',
+            'WFEmailAddressContentItem',
+            'WFGenericFileContentItem',
+            'WFImageContentItem',
+            'WFiTunesProductContentItem',
+            'WFLocationContentItem',
+            'WFDCMapsLinkContentItem',
+            'WFAVAssetContentItem',
+            'WFPDFContentItem',
+            'WFPhoneNumberContentItem',
+            'WFRichTextContentItem',
+            'WFSafariWebPageContentItem',
+            'WFStringContentItem',
+            'WFURLContentItem'
+        ]
     };
 }
 
 function generateShortcut() {
     const shortcut = createShortcut();
     const plistData = plist.build(shortcut);
-    const bplistData = bplist(plistData);
+    fs.writeFileSync('WiFiQRShortcut.plist', plistData);
+    console.log('Shortcut plist file "WiFiQRShortcut.plist" has been created.');
+
+    const bplistData = bplist(shortcut);
     fs.writeFileSync('WiFiQRShortcut.shortcut', bplistData);
     console.log('Shortcut file "WiFiQRShortcut.shortcut" has been created.');
 }
